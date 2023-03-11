56351e4f6008e34d33371700f3f41551dc1a91e5
supraja-maddineni <suprajamaddineni345@gmail.com>
2023-03-11 12:22:06 +0530

    first commit

---
+++ app2/urls.py
@@ -0,0 +1,6 @@
+from app2.views import *
+from django.urls import path
+app_name='nothing'
+urlpatterns=[
+    path('First_app2',First_app2,name='First_app2'),
+]
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def First_app2(request):
+    return render(request,'first.html')
